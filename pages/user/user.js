db09abcb8201aa89e681cf3905c198108c625fd7
张三 <13245209+lzt_luo@user.noreply.gitee.com>
2024-10-15 01:42:13 +0800

    添加个人用户界面

--- pages/user/user.js
+++ pages/user/user.js
@@ -1,13 +1,49 @@
 Page({
   data: {
     userInfo: {
-      avatar: "",
+      avatar:
+        "http://119.91.145.137:9001/choose/2024/06/03/7595dce18c3440208c55204af85b8430.png",
       nickname: "用户名",
     },
     userTags: ["运动", "健康饮食", "瑜伽"],
     healthTip: "每天喝8杯水有助于保持身体水分平衡，促进新陈代谢。",
     recommendations: [
-      { image: "", title: "10分钟早晨瑜伽" },
+      {
+        image: "../../images/食物.png",
+        tag: "主菜",
+        name: "红烧肉",
+        distance: "20km",
+      },
+      {
+        image: "../../images/食物1.png",
+        tag: "甜品",
+        name: "水果沙拉",
+        distance: "10km",
+      },
+      {
+        image: "../../images/食物.png",
+        tag: "糕点",
+        name: "蛋糕",
+        distance: "5km",
+      },
+      {
+        image: "../../images/食物1.png",
+        tag: "主菜",
+        name: "红烧肉",
+        distance: "20km",
+      },
+      {
+        image: "../../images/食物.png",
+        tag: "甜品",
+        name: "水果沙拉",
+        distance: "10km",
+      },
+      {
+        image: "../../images/食物1.png",
+        tag: "糕点",
+        name: "蛋糕",
+        distance: "5km",
+      },
     ],
   },
 
@@ -33,6 +69,13 @@ Page({
     // 跳转到关于我们页面
     wx.navigateTo({
       url: "/pages/about_us/about_us",
+    });
+  },
+
+  navigateToSettings: function () {
+    // 跳转到设置页面
+    wx.navigateTo({
+      url: "/pages/settings/settings",
     });
   },
 });
